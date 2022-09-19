129956073b47ae63ced412ab67bc1385bb596af3
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-20 00:05:20 +0200

    implemented tailwindcss with custom color vars

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    darkMode: "class",
+    theme: {
+      extend: {
+        backgroundColor: {
+            primary: "var(--color-bg-primary)",
+            secondary: "var(--color-bg-secondary)",
+            button: "var(--color-bg-button)",
+        },
+        textColor: {
+            primary: "var(--color-text-primary)",
+            secondary: "var(--color-text-secondary)",
+            accent: "var(--color-text-accent)",
+            button: "var(--color-bg-secondary)",
+        },
+        borderColor: {
+            primary: "var(--color-bg-primary)",
+            secondary: "var(--color-bg-secondary)",
+            accent: "var(--color-text-accent)",
+            input: "var(--color-bg-input)",
+        },
+      },
+    },
+    plugins: [],
+  }
