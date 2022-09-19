f2973078ce6747845cd25cb53ed9b35fcf9131bd
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-20 00:29:33 +0200

    Added ThemeContext

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect, useContext, createContext } from "react"
+
+// Saved theme
+export const getInitialTheme = () => {
+    if (typeof window !== "undefined" && window.localStorage) {
+        const storedRefs = window.localStorage.getItem("color-theme")
+        if (typeof storedRefs === "string") {
+            return storedRefs
+        }
+        
+        const userMedia = window.matchMedia("(prefers-color-scheme: dark)")
+        if (userMedia.matches) {
+            return "dark"
+        }
+    }
+    return "light"
+}
+
+// Context
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({initialTheme, children}) => {
+    const [theme, setTheme] = useState(getInitialTheme)
+    const rawSetTheme = (theme) => {
+        const root = window.document.documentElement
+        const isDark = theme === "dark"
+
+        root.classList.remove(isDark ? "light" : "dark")
+        root.classList.add(theme)
+
+        localStorage.setItem("color-theme", theme)
+    }
+
+    if (initialTheme) {
+        rawSetTheme(initialTheme)
+    }
+    
+    useEffect(() => {
+        rawSetTheme(theme)
+    }, [theme]);
+
+ 
+    return (
+        <ThemeContext.Provider value={{
+            theme,
+            setTheme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useThemeContext = () => useContext(ThemeContext)
